2aaef8a37e9d706e3a56b909ebbe20cdb7766905
InventerBots <luke@ebarnfarm.com>
2022-02-13 12:31:07 -0600

    Working TCP server

---
+++ Rpi/Python/server.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = '169.254.80.147' # server IP
+PORT = 1024
+
+msg = b'Welcome!'
+
+with socket.socket() as s:
+  s.bind((HOST, PORT))
+  s.listen()
+  conn, addr = s.accept()
+  with conn:
+    print('Connected by', addr)
+    while True:
+      data = conn.recv(1024)
+      if not data:
+        break
+      print("Receved: ", data)
+      print("Sent: ", msg)
+      conn.sendall(msg)
+
