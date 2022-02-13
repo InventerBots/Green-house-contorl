02d7ab8bb39748fae48ada41e063c588ad90426e
InventerBots <luke@ebarnfarm.com>
2022-02-13 12:44:24 -0600

    working TCP echo client

--- Rpi/Python/basicClient.py
+++ Rpi/Python/basicClient.py
@@ -1,8 +1,14 @@
 import socket               
 
-s = socket.socket(socket.AF_INET)        
-host = '169.254.1.177'# ip of raspberry pi 
-port = 1024               
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
+HOST = '169.254.80.147' # server IP
+PORT = 1028
+
+msg = b'Hello, World!'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Sent,", msg)
+    s.connect((HOST, PORT))
+    s.sendall(msg)
+    data = s.recv(1024)
+
+print('Received', repr(data))
