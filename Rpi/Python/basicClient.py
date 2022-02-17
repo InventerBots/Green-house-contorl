6dd3d1e1830806d53e8a181e00c5f80cb8bd20a9
Luke <luke@ebarnfarm.com>
2022-02-16 21:28:26 -0600

    Update basicClient.py
    experimentation to get a connection with Arduino

--- Rpi/Python/basicClient.py
+++ Rpi/Python/basicClient.py
@@ -1,11 +1,11 @@
 import socket               
 
 HOST = '169.254.80.147' # server IP
-PORT = 1028
+PORT = 2046
 
 msg = b'Hello, World!'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket() as s:
     print("Sent,", msg)
     s.connect((HOST, PORT))
     s.sendall(msg)
