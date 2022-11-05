b9f8484c89b755d2539e5fce8a00be1824a9f47f
Luke <luke@ebarnfarm.com>
2022-11-04 21:59:00 -0500

    Properly added PyQt

--- Rpi/Python/gui_res.py
+++ Rpi/Python/gui_res.py
@@ -1,6 +1,19 @@
-import PyQt5
-import server
-
-
-def test():
-    server.connect()
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+class window(QWidget):
+   def __init__(self, parent = None):
+      super(window, self).__init__(parent)
+      self.resize(200,50)
+      self.setWindowTitle("PyQt5")
+      self.label = QLabel(self)
+      self.label.setText("Hello World")
+      self.label.move(50,20)
+def main():
+   app = QApplication(sys.argv)
+   ex = window()
+   ex.show()
+   sys.exit(app.exec_())
+if __name__ == '__main__':
+   main()
