2c68334d057da8564f506f35bd9a1987d3e3fe32
Luke <luke@ebarnfarm.com>
2023-03-03 05:09:10 -0600

    created new server

---
+++ Rpi/Python/testing/serverTest.py
@@ -0,0 +1,44 @@
+import socket
+import time
+
+class TCPServer:
+    def __init__(self, host='192.168.1.50', port=10004):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        
+        self.tempRaw_12bit_int = []
+
+    def accept(self):
+        print('Server listening on {}:{}'.format(self.host, self.port))
+        return self.server_socket.accept()
+        
+    def openConnection(self, socket, sensors_to_read):
+        if type(socket) is not tuple:
+            raise Exception('Socket not doses not exist!')
+        client_socket, client_address = socket
+        self.tempRaw_12bit_int.clear()
+        
+        for ind in range(1, sensors_to_read+1):
+            client_socket.send(ind.to_bytes(2, 'big'))
+            self.tempRaw_12bit_int.append(int(client_socket.recv(4096)))
+        print(self.tempRaw_12bit_int)
+        
+    def disconnect(self):
+        print('Server shutting down')
+        self.server_socket.shutdown(1)
+        self.server_socket.close()
+
+if __name__ == '__main__':
+    server = TCPServer()
+    try:
+        client_connection = server.accept()
+        for x in range(0, 30):
+            server.openConnection(client_connection, 3)
+            time.sleep(0.25)
+        server.disconnect()
+    except KeyboardInterrupt:
+        server.disconnect()
