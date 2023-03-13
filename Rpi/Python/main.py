e23edff52025dad19adb58450585109d2f6dcb5b
Luke <luke@ebarnfarm.com>
2023-03-12 23:40:39 -0500

    Added progress bar for building data set

--- Rpi/Python/main.py
+++ Rpi/Python/main.py
@@ -25,6 +25,8 @@ class ServerThread(QThread):
         self.conn_tup = None
         self.tempBuff = deque([])
         self.tempRaw_12bit = []
+        self.logset_rise = deque([])
+        self.buff = 7200 # number of readings to keep, 7200 = 30s of data
         
     def run(self):
         self.server_obj = server.TCPServer()
@@ -39,6 +41,28 @@ class ServerThread(QThread):
         if len(self.tempBuff) > 2:
             self.tempBuff.popleft()
                
+        '''
+        Calculate temperature rise
+        
+        ''' 
+        try:
+            cTemp = self.tempBuff[0]
+            pTemp = self.tempBuff[1]
+            
+            for c in range(len(cTemp)):
+                cTemp_deg = numpy.array(tempCalc.convertRawToDeg_F(cTemp[c]))
+            for p in range(len(pTemp)):
+                pTemp_deg = numpy.array(tempCalc.convertRawToDeg_F(pTemp[p]))
+            rise = numpy.subtract(pTemp_deg, cTemp_deg)
+            
+            self.logset_rise.append(rise)
+            if len(self.logset_rise) > self.buff:
+                self.logset_rise.popleft()
+                
+            print(len(self.logset_rise))
+        except:
+            pass
+        
     def connect(self):
         if not self.conn_tup and not self.is_connected:
             self.conn_tup = self.server_obj.acceptConnection()
@@ -61,6 +85,7 @@ class MainWindow(QMainWindow):
         
         self.display_val = []
         self.display_ind = 0
+        self.buff = 0
         self.server_thread = None
         
     def initUI(self):
@@ -69,13 +94,6 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("background-color : rgb(47, 62, 67)")
         # self.setMinimumSize(800, 480)
         
-        mainLayout = QGridLayout()
-        dataLayout_main = QHBoxLayout()
-        dataLayout_col1 = QFormLayout()
-        dataLayout_col2 = QFormLayout()
-        btnLayout = QHBoxLayout()
-        fanLayout = QVBoxLayout()
-
         self.val_1_label = QLabel("1")
         self.val_1_label.setFont(QFont('Default', 20))
         self.val_1_label.setStyleSheet("color : rgb(128, 128, 128)")
@@ -152,18 +170,33 @@ class MainWindow(QMainWindow):
         self.l_rise_1.setFont(QFont('Default', 25))
         self.l_rise_1.setStyleSheet("color : rgb(68, 81, 86)")
 
-        b_Connect = QPushButton("Start server")
-        b_Connect.setFont(QFont('Default', 15))
-        b_Connect.setStyleSheet("background-color : rgb(21, 93, 197); color : white")
-        b_Connect.setMinimumHeight(50)
-        b_Connect.clicked.connect(self.startServer)
+        self.b_Connect = QPushButton("Start server")
+        self.b_Connect.setFont(QFont('Default', 15))
+        self.b_Connect.setStyleSheet("background-color : rgb(21, 93, 197); color : white")
+        self.b_Connect.setMinimumHeight(50)
+        self.b_Connect.clicked.connect(self.startServer)
         
-        b_Disconnect = QPushButton("Stop server")
-        b_Disconnect.setFont(QFont('Default', 15))
-        b_Disconnect.setStyleSheet("background-color : rgb(21, 93, 197); color : white")
-        b_Disconnect.setMinimumHeight(50)
-        b_Disconnect.clicked.connect(self.stopServer)
-
+        self.b_Disconnect = QPushButton("Stop server")
+        self.b_Disconnect.setFont(QFont('Default', 15))
+        self.b_Disconnect.setStyleSheet("background-color : rgb(21, 93, 197); color : white")
+        self.b_Disconnect.setMinimumHeight(50)
+        self.b_Disconnect.clicked.connect(self.stopServer)
+        
+        self.pb_Dataset = QProgressBar(self)
+        self.pb_Dataset.setRange(0, 7200)
+        
+        '''
+        Layoyts
+        
+        '''
+        mainLayout = QGridLayout()
+        dataLayout_main = QHBoxLayout()
+        dataLayout_col1 = QFormLayout()
+        dataLayout_col2 = QFormLayout()
+        dataBarLayout = QVBoxLayout()
+        btnLayout = QHBoxLayout()
+        fanLayout = QVBoxLayout()
+        
         dataLayout_col1.addRow(self.val_1_label, self.l_Val_1)
         dataLayout_col1.addRow(self.val_3_label, self.l_Val_3)
         dataLayout_col1.addRow(self.val_5_label, self.l_Val_5)
@@ -173,11 +206,13 @@ class MainWindow(QMainWindow):
 
         dataLayout_main.addLayout(dataLayout_col1)
         dataLayout_main.addLayout(dataLayout_col2)
-        btnLayout.addWidget(b_Connect)
-        btnLayout.addWidget(b_Disconnect)
-
+        dataBarLayout.addWidget(self.pb_Dataset)
+        btnLayout.addWidget(self.b_Connect)
+        btnLayout.addWidget(self.b_Disconnect)
+        
         mainLayout.addLayout(dataLayout_main, 0, 0)
-        mainLayout.addLayout(btnLayout, 1, 0)
+        mainLayout.addLayout(dataBarLayout, 1, 0)
+        mainLayout.addLayout(btnLayout, 2, 0)
         mainLayout.addLayout(fanLayout, 0, 2, 1, 2)
 
         lay = QWidget()
@@ -197,6 +232,7 @@ class MainWindow(QMainWindow):
             self.server_thread = ServerThread()
             self.server_thread.start()
             self.timer.start()
+            self.buff = self.server_thread.buff
             
             self.l_Val_1.setStyleSheet("color : rgb(255, 255, 255)")
             self.l_Val_2.setStyleSheet("color : rgb(255, 255, 255)")
@@ -210,6 +246,7 @@ class MainWindow(QMainWindow):
             return -1
         self.server_thread.mainLoop()
         
+        self.pb_Dataset.setValue(len(self.server_thread.logset_rise))
         if self.display_ind < 4:
             self.display_val.append(self.server_thread.tempBuff[0])
             self.display_ind += 1
@@ -237,7 +274,7 @@ class MainWindow(QMainWindow):
         self.l_Val_5.setStyleSheet("color : rgb(68, 81, 86)")
         self.l_Val_6.setStyleSheet("color : rgb(68, 81, 86)")
         
-if __name__ == '__name__':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
